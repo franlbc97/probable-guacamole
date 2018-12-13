9e377da18d5b420a1dee5a8cb63a9c44cea7da29
franorris <franlbc97@gmail.com>
2018-12-13 16:36:51 +0100

    Add files via upload

--- src/WallComponent.cpp
+++ src/WallComponent.cpp
@@ -1,7 +1,8 @@
 #include "WallComponent.h"
 
-WallComponent::WallComponent()
+WallComponent::WallComponent(FMOD::Geometry * geometrys):_geo(_geo)
 {
+	
 }
 
 WallComponent::~WallComponent()
@@ -10,6 +11,23 @@ WallComponent::~WallComponent()
 
 bool WallComponent::init(AppObject * obj)
 {
+	FMOD_VECTOR verts[4];
+	verts[0].x = 0;
+	verts[0].y = 100;
+	verts[0].z = 500;
+
+	verts[1].x = 0;
+	verts[1].y = -100;
+	verts[1].z = 500;
+
+	verts[2].x = 1000;
+	verts[2].y = -100;
+	verts[2].z = 500;
+
+	verts[3].x = 1000;
+	verts[3].y = 100;
+	verts[3].z = 500;
+	_geo->addPolygon(directOclusion, reverbOcclusion, true, 4, verts, &polygonIndex);
 	return false;
 }
 
--- src/WallComponent.h
+++ src/WallComponent.h
@@ -1,17 +1,23 @@
 #pragma once
 #include "Component.h"
+#include <fmod.hpp>
 class WallComponent:public Component
 {
 public:
-	WallComponent();
+	WallComponent(FMOD::Geometry * geometry);
 	~WallComponent();
 	virtual bool init(AppObject * obj);
 	virtual void render(SDL_Renderer * r, AppObject * obj);
 	virtual void tick(AppObject * obj);
 	virtual bool handleInput(SDL_Event & e, AppObject * obj);
 	virtual void release(AppObject * obj);
+	
 
 private:
+	FMOD::Geometry* _geo;
+	int polygonIndex;
+	float directOclusion;
+	float reverbOcclusion;
 
 };
 
