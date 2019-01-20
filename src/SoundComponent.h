b6b8679ac41a2732160cf252429b05532b13d471
franlbc97 <franlbc97@gmail.com>
2019-01-20 01:50:09 +0100

    First iteration of documentation finished

--- src/SoundComponent.h
+++ src/SoundComponent.h
@@ -13,22 +13,67 @@ public:
 		file_ = file;
 	}
 
+	/// <summary>
+	/// The destructor of the Component
+	/// </summary>
 	virtual ~SoundComponent() { sound_->release(); }
 
-	
+	/// <summary>
+	/// Initializes the sound
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
+	/// <returns>True if something fails, false in every other case</returns>
 	virtual bool init(AppObject * obj);
+	/// <summary>
+	/// Render when thee sund is hearable by the listener, and the volume of the sound
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
 	virtual void render(AppObject * obj);
+	/// <summary>
+	/// Updates the position of the sound and the orientation
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
 	virtual void tick(AppObject * obj);
+	/// <summary>
+	/// Handles the input of the component
+	/// </summary>
+	/// <param name="e">The event</param>
+	/// <param name="obj">The object that is asociated with component</param>
+	/// <returns></returns>
 	virtual bool handleInput(SDL_Event & e, AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="obj"></param>
 	virtual void release(AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
 	virtual const char * getType();
+	/// <summary>
+	/// Toggles the pause and the pause
+	/// </summary>
 	void togglePause();
 
-
+	/// <summary>
+	/// Changes the position of the sound
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <param name="z"></param>
 	void changePosition3D(const int & x, const int & y, const int & z);
 
+	/// <summary>
+	/// Sets the volume of the sounds volume
+	/// </summary>
+	/// <param name="nVol">The new volume value</param>
 	void setVolume(const float & nVol);
 
+	/// <summary>
+	/// For getting the value of the volume
+	/// </summary>
+	/// <returns>the value of the volume</returns>
 	inline float getVolume() {
 		float vol;
 		channelSound_->getVolume(&vol);
--- src/WallComponent.h
+++ src/WallComponent.h
@@ -4,14 +4,45 @@
 class WallComponent:public Component
 {
 public:
-
+	/// <summary>
+	/// Constructor of the component
+	/// </summary>
+	/// <param name="directOclusion">The direct occlusion of the wall</param>
+	/// <param name="reverbOcclusion">The reverb occlusion of the wal</param>
 	WallComponent(const float & directOclusion,const float & reverbOcclusion);
 	~WallComponent();
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
+	/// <returns></returns>
 	virtual bool init(AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
 	virtual void render( AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
 	virtual void tick(AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="e">The event</param>
+	/// <param name="obj">The object that is asociated with component</param>
+	/// <returns></returns>
 	virtual bool handleInput(SDL_Event & e, AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="obj">The object that is asociated with component</param>
 	virtual void release(AppObject * obj);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
 	virtual const char * getType();
 
 	
